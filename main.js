d77d9e29eaf0ee6cc16015ec8efe1106179402ac
Amathost <124377028+Amathost@users.noreply.github.com>
2024-07-18 11:45:46 -0400

    Filled in everything, need pics

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 // navbar.js
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/navbar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('navbar-container').innerHTML = data;
-      });
+  fetch('/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('navbar-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error fetching navbar:', error));
 
-    fetch('/footer.html')
+  fetch('/footer.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('footer-container').innerHTML = data;
     })
-  });
+    .catch(error => console.error('Error fetching footer:', error));
+});
 
 let slideIndex = 0;
 
