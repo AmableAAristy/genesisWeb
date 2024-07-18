5bbecf79b09f94911e2c5f192aac9e5014187b45
Amathost <124377028+Amathost@users.noreply.github.com>
2024-07-18 16:08:19 -0400

    sorting some issues with relative pathing

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // navbar.js
 document.addEventListener('DOMContentLoaded', function() {
-  fetch('/navbar.html')
+  fetch('navbar.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('navbar-container').innerHTML = data;
     })
     .catch(error => console.error('Error fetching navbar:', error));
 
-  fetch('/footer.html')
+  fetch('footer.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('footer-container').innerHTML = data;
